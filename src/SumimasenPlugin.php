104472b9e55c10c3796303c64bffd77cb35068ce
Henry <arisman.henry@gmail.com>
2025-06-15 07:47:37 +0700

    Update register and boot

--- src/SumimasenPlugin.php
+++ src/SumimasenPlugin.php
@@ -15,6 +15,19 @@ use Littleboy130491\Sumimasen\Filament\Resources\SubmissionResource;
 use Littleboy130491\Sumimasen\Filament\Resources\TagResource;
 use Littleboy130491\Sumimasen\Filament\Resources\UserResource;
 
+
+use Awcodes\Curator\CuratorPlugin;
+use Jeffgreco13\FilamentBreezy\BreezyCore;
+use Illuminate\Validation\Rules\Password;
+use Outerweb\FilamentTranslatableFields\Filament\Plugins\FilamentTranslatableFieldsPlugin;
+use Filament\Navigation\NavigationGroup;
+use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
+use Datlechin\FilamentMenuBuilder\FilamentMenuBuilderPlugin;
+use Illuminate\Support\Facades\Gate;
+use Spatie\ResponseCache\Middlewares\DoNotCacheResponse;
+use ShuvroRoy\FilamentSpatieLaravelBackup\FilamentSpatieLaravelBackupPlugin;
+use Filament\Forms\Components\TextInput;
+
 class SumimasenPlugin implements Plugin
 {
     use EvaluatesClosures;
@@ -34,7 +47,43 @@ class SumimasenPlugin implements Plugin
     {
         $panel
             ->resources($this->getResources())
-            ->pages($this->getPages());
+            ->pages($this->getPages())
+            ->plugins([
+                CuratorPlugin::make(),
+                BreezyCore::make()
+                    ->myProfile(
+                        shouldRegisterNavigation: true,
+                        navigationGroup: 'Users'
+                    )
+                    ->passwordUpdateRules(
+                        rules: [Password::default()->mixedCase()->uncompromised(3)], // you may pass an array of validation rules as well. (default = ['min:8'])
+                        requiresCurrentPassword: true, // when false, the user can update their password without entering their current password. (default = true)
+                    ),
+                FilamentTranslatableFieldsPlugin::make(),
+                FilamentShieldPlugin::make(),
+                FilamentMenuBuilderPlugin::make()
+                    ->showCustomTextPanel()
+                    ->addLocations($this->getMenuLocations())
+                    ->addMenuItemFields([
+                        TextInput::make('classes'),
+                    ]),
+                FilamentSpatieLaravelBackupPlugin::make()
+                    ->authorize(fn(): bool => auth()->user()->hasRole(['admin', 'super_admin'])),
+            ])
+            ->navigationGroups([
+                NavigationGroup::make()
+                    ->label('Contents')
+                    ->icon('heroicon-o-document-text'),
+                NavigationGroup::make()
+                    ->label('Users')
+                    ->icon('heroicon-o-users'),
+                NavigationGroup::make()
+                    ->label('Profile')
+                    ->icon('heroicon-o-cog-6-tooth'),
+                NavigationGroup::make()
+                    ->label('Settings')
+                    ->icon('heroicon-o-cog-6-tooth'),
+            ]);
     }
 
     public function boot(Panel $panel): void
@@ -43,6 +92,8 @@ class SumimasenPlugin implements Plugin
             ->unsavedChangesAlerts()
             ->sidebarCollapsibleOnDesktop()
             ->databaseNotifications();
+        Gate::policy(\Awcodes\Curator\Models\Media::class, \App\Policies\MediaPolicy::class);
+        Gate::policy(\Datlechin\FilamentMenuBuilder\Models\Menu::class, \App\Policies\MenuPolicy::class);
     }
 
     public static function make(): static
@@ -104,5 +155,24 @@ class SumimasenPlugin implements Plugin
     public function hasSettingsPage(): bool
     {
         return $this->hasSettingsPage;
+    }
+
+    private function getMenuLocations(): array
+    {
+        $languages = config('cms.language_available', []);
+        $locations = [];
+
+        $baseLocations = config('cms.navigation_menu_locations', [
+            'header' => 'Header',
+            'footer' => 'Footer',
+        ]);
+
+        foreach ($baseLocations as $key => $label) {
+            foreach ($languages as $langCode => $langName) {
+                $locations["{$key}_{$langCode}"] = "{$label} ({$langName})";
+            }
+        }
+
+        return $locations;
     }
 }
