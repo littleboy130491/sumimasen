f9b4c2b1c6df4fc83a6dac47db39c5d21fb4b0ea
Henry <littleboy_13@yahoo.com>
2025-07-22 17:48:20 +0700

    Change section to block

--- src/Traits/HasSections.php
+++ src/Traits/HasSections.php
@@ -2,53 +2,58 @@
 
 namespace Littleboy130491\Sumimasen\Traits;
 
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Awcodes\Curator\Models\Media;
 
 trait HasSections
 {
-    protected function section(): Attribute
+    /**
+     * Get sections with fallback and media URLs for frontend
+     */
+    public function getSectionsForFrontend(): array
     {
-        return Attribute::make(
-            get: function ($value, $attributes) {
-                // Get the raw JSON translations from attributes
-                $translations = json_decode($attributes['section'] ?? '{}', true);
+        // Get all translations for the section field
+        $translations = $this->getTranslations('section');
 
-                // Get current locale
-                $currentLocale = $this->getLocale();
+        // Get current locale
+        $currentLocale = app()->getLocale();
 
-                // Get current locale's value
-                $currentValue = $translations[$currentLocale] ?? [];
+        // Get current locale's value
+        $currentValue = $translations[$currentLocale] ?? [];
 
-                // If current locale is empty, use fallback
-                if (empty($currentValue)) {
-                    // Try default language
-                    $defaultLocale = config('cms.default_language');
+        // If current locale is empty, use fallback
+        if (empty($currentValue)) {
+            // Try default language
+            $defaultLocale = config('cms.default_language', config('app.fallback_locale'));
 
-                    if (isset($translations[$defaultLocale]) && !empty($translations[$defaultLocale])) {
-                        $currentValue = $translations[$defaultLocale];
-                    } else {
-                        // Return first non-empty translation
-                        foreach ($translations as $locale => $localeValue) {
-                            if (!empty($localeValue)) {
-                                $currentValue = $localeValue;
-                                break;
-                            }
-                        }
+            if (isset($translations[$defaultLocale]) && !empty($translations[$defaultLocale])) {
+                $currentValue = $translations[$defaultLocale];
+            } else {
+                // Return first non-empty translation
+                foreach ($translations as $locale => $localeValue) {
+                    if (!empty($localeValue)) {
+                        $currentValue = $localeValue;
+                        break;
                     }
                 }
-
-                // Inject media URLs into blocks
-                return collect($currentValue)->map(function (array $block) {
-                    // if this block has an "image" key, fetch its URL
-                    if (isset($block['data']['image'])) {
-                        $media = Media::find($block['data']['image']);
-                        $block['data']['media_url'] = $media?->url;
-                    }
-
-                    return $block;
-                })->all();
             }
-        );
+        }
+
+        // Inject media URLs into blocks
+        return collect($currentValue)->map(function (array $block) {
+            if (isset($block['data']['image'])) {
+                $media = Media::find($block['data']['image']);
+                $block['data']['media_url'] = $media?->url;
+            }
+            return $block;
+        })->all();
     }
+
+    /**
+     * Alias for frontend sections as 'block'
+     */
+    public function getBlockAttribute(): array
+    {
+        return $this->getSectionsForFrontend();
+    }
+
 }
