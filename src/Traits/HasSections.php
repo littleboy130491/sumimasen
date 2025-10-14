6638e507012103255fc8d5ed989a4a6929abc5f4
unknown <littleboy_13@yahoo.com>
2025-10-14 16:17:31 +0700

    Fix multiple images

--- src/Traits/HasSections.php
+++ src/Traits/HasSections.php
@@ -40,33 +40,59 @@ trait HasSections
 
         // Inject media URLs into blocks
         return collect($currentValue)->map(function (array $block) {
-            // Handle single image
-            if (isset($block['data']['image'])) {
+            // Handle single image (integer ID from CuratorPicker)
+            if (isset($block['data']['image']) && is_int($block['data']['image'])) {
                 $media = Media::find($block['data']['image']);
                 $block['data']['media_url'] = $media?->url;
             }
 
-            // Handle single media
-            if (isset($block['data']['media'])) {
+            // Handle single media field
+            if (isset($block['data']['media']) && is_int($block['data']['media'])) {
                 $media = Media::find($block['data']['media']);
                 $block['data']['media_url'] = $media?->url;
             }
 
-            // Handle multiple images (gallery)
+            // Handle logo field (for tab blocks)
+            if (isset($block['data']['logo']) && is_int($block['data']['logo'])) {
+                $logoMedia = Media::find($block['data']['logo']);
+                $block['data']['logo_url'] = $logoMedia?->url;
+            }
+
+            // Handle gallery with embedded media objects (from CuratorPicker with multiple())
+            // This comes as an object with UUID keys containing full media data
+            if (isset($block['data']['image']) && is_array($block['data']['image']) && !empty($block['data']['image'])) {
+                // Check if it's an associative array with UUID keys (gallery format)
+                $firstKey = array_key_first($block['data']['image']);
+                if ($firstKey && !is_numeric($firstKey)) {
+                    // Extract URLs from embedded media objects
+                    $block['data']['gallery_urls'] = collect($block['data']['image'])
+                        ->pluck('url')
+                        ->filter()
+                        ->values()
+                        ->toArray();
+                }
+            }
+
+            // Handle multiple images using gallery array (array of IDs)
             if (isset($block['data']['gallery']) && is_array($block['data']['gallery'])) {
                 $mediaIds = $block['data']['gallery'];
                 $mediaItems = Media::whereIn('id', $mediaIds)->get()->keyBy('id');
 
                 // Map gallery IDs to their URLs, preserving order
-                $block['data']['gallery_urls'] = collect($mediaIds)
+                $galleryUrls = collect($mediaIds)
                     ->map(fn($id) => $mediaItems->get($id)?->url)
                     ->filter() // Remove null values
                     ->values()
-                    ->all();
+                    ->toArray();
+
+                // Only override if we don't already have gallery_urls from image field
+                if (empty($block['data']['gallery_urls'])) {
+                    $block['data']['gallery_urls'] = $galleryUrls;
+                }
             }
 
             return $block;
-        })->all();
+        })->toArray();
     }
 
     /**
