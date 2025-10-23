d753013052ffc22e2d5295f84f88161b3bf875a9
unknown <littleboy_13@yahoo.com>
2025-10-23 16:20:22 +0700

    Update route so it won't intercept route from impersonate and filament export

--- routes/web.php
+++ routes/web.php
@@ -66,7 +66,7 @@ Route::get('/{path}', function (Illuminate\Http\Request $request, $path) {
     return redirect()->to($redirectUrl);
 })
     ->middleware(['setLocale', 'web'])
-    ->where('path', '^(?!(?:admin|filament|filament-impersonate|storage|up|' . implode('|', array_keys(Config::get('cms.language_available', ['en' => 'English']))) . ')(?:/|$)).*');
+    ->where('path', '^(?!(?:admin|filament|filament-impersonate|up|' . implode('|', array_keys(Config::get('cms.language_available', ['en' => 'English']))) . ')(?:/|$)).*');
 
 Route::prefix('{lang}')
     ->whereIn('lang', array_keys(Config::get('cms.language_available', ['en' => 'English'])))
